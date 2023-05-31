53988bc4b9876a781986e974a0867cb24f0b4d5c
ashepard03 <ashepard@appdev.yearup.org>
2023-05-31 10:37:46 -0400

    update

--- src/main/java/org/example/Topping.java
+++ src/main/java/org/example/Topping.java
@@ -4,13 +4,17 @@ public class Topping {
     private String name;
     private boolean isMeat;
     private boolean isCheese;
-    private double price;
+    private double smallPrice;
+    private double medPrice;
+    private double lgPrice;
 
-    public Topping(String name, boolean isMeat, boolean isCheese, double price) {
+    public Topping(String name, boolean isMeat, boolean isCheese, double smallPrice, double medPrice, double lgPrice) {
         this.name = name;
         this.isMeat = isMeat;
         this.isCheese = isCheese;
-        this.price = price;
+        this.smallPrice = smallPrice;
+        this.medPrice = medPrice;
+        this.lgPrice = lgPrice;
     }
 
     public String getName() {
