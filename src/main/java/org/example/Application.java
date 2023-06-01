6c601b39ff2e7317f534f532251fc695026681bc
Cristi Savca <58373811+CristiSavca@users.noreply.github.com>
2023-06-01 11:06:00 -0400

    overwrite heralda's application

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,20 +1,15 @@
 package org.example;
 
 
-
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
-    private Menu menu;
-    private Order[] orders;
-
-    public Application() {
-        menu = new Menu();
-
-    }
+    public Scanner scanner = new Scanner(System.in);
+    public SandwichSize sandwichSize = null;
+    OrderFileManager OFM = new OrderFileManager();
 
     public void screen() {
-        Scanner scanner = new Scanner(System.in);
         int option;
 
         do {
@@ -48,7 +43,6 @@ public class Application {
 
     private void orderScreen() {
         Order order = new Order();
-        Scanner scanner = new Scanner(System.in);
         int option;
 
         do {
@@ -63,7 +57,7 @@ public class Application {
                     addDrink(order);
                     break;
                 case 3:
-                    addChips(order);
+                    addChip(order);
                     break;
                 case 4:
                     checkoutOrder(order);
@@ -91,21 +85,74 @@ public class Application {
         System.out.print("Enter your option: ");
     }
 
+    private void addSandwich(Order order) {
+        System.out.println("Creating Sandwich...");
+        System.out.println("Select your bread: ");
+        System.out.println("[1] White");
+        System.out.println("[2] Wheat");
+        System.out.println("[3] Rye");
+        System.out.println("[4] Wrap");
+        System.out.print("Enter your choice: ");
+        int breadChoice = scanner.nextInt();
+        scanner.nextLine();
+
+        Bread bread = null;
+        switch (breadChoice) {
+            case 1 -> bread = Bread.WHITE;
+            case 2 -> bread = Bread.WHEAT;
+            case 3 -> bread = Bread.RYE;
+            case 4 -> bread = Bread.WRAP;
+            default -> System.out.println("Invalid bread choice. Please try again");
+        }
+
+        System.out.println("Select your sandwich size: ");
+        System.out.println("[1] 4 inch");
+        System.out.println("[2] 8 inch");
+        System.out.println("[3] 12 inch");
+        System.out.print("Enter your choice: ");
+        int sandwichChoice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (sandwichChoice) {
+            case 1 -> sandwichSize = SandwichSize.FOUR_IN;
+            case 2 -> sandwichSize = SandwichSize.EIGHT_IN;
+            case 3 -> sandwichSize = SandwichSize.TWELVE_IN;
+            default -> System.out.println("Invalid bread choice. Please try again");
+        }
+
+        //display the list of toppings
+        System.out.println(toppingMenu);;
+        //allow the user to enter their toppings of choice
+        System.out.println("Select toppings (separate with comma, e.g., Lettuce, Extra meat, Provolone): ");
+        String toppingsInput = scanner.nextLine();
+        String[] toppingsArray = toppingsInput.split(",");
+
+        Sandwich customizedSandwich = new Sandwich(bread, sandwichSize);
+
+        for (String topping : toppingsArray) {
+            Topping t = isToppingValid(topping);
+            if (t != null) {
+                customizedSandwich.addTopping(t);
+            }
+        }
+
+        System.out.println("Would you like yur sandwich toasted? (yes/no)");
+        System.out.print("Enter your choice: ");
+        String toastedChoice = scanner.nextLine();
+
+        if (toastedChoice.equalsIgnoreCase("Yes")) {
+            customizedSandwich.setToasted(true);
+        } else {
+            customizedSandwich.setToasted(false);
+        }
+
+        order.addSandwich(customizedSandwich);
+        System.out.println("The following sandwich has been added to your order\n" + customizedSandwich.toString());
+    }
+
+
     private void addDrink(Order order) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Add Drink");
-        System.out.println("=========");
-
-        System.out.println("Drink name: ");
-        String name = scanner.nextLine();
-
-        System.out.println("Drink size: ");
-        int size = scanner.nextInt();
-
-        double price = menu.getDrinkPrice(size);
-        Drink drink = new Drink(name, size, price);
-        order.addDrink(drink);
+        DrinksMenu.runDrinkMenu();
 
         System.out.println("Drink added.");
     }
@@ -114,10 +161,8 @@ public class Application {
         System.out.println("Checkout");
         System.out.println("========");
 
-        order.displayOrderDetails();
-        System.out.println("Total cost: $" + order.TotalCost());
+        OFM.generateReceipt(order);
 
-        Scanner scanner = new Scanner(System.in);
         int option;
 
         do {
@@ -128,7 +173,7 @@ public class Application {
 
             switch (option) {
                 case 1:
-                    order.saveReceipt();
+                    OFM.saveReceipt(order);
                     System.out.println("Order confirmed. Receipt created.");
                     return;
                 case 2:
@@ -144,8 +189,6 @@ public class Application {
     }
 
     private void addChip(Order order) {
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("Add Chips");
         System.out.println("=========");
 
@@ -158,6 +201,49 @@ public class Application {
         order.addChip(chip);
 
         System.out.println("Chips added.");
+    }
+
+    String toppingMenu = """
+                    Meats:
+                        - Steak
+                        - Ham
+                        - Salami
+                        - Roast Beef
+                        - Chicken
+                        - Bacon
+                    Cheese:
+                        - American
+                        - Provolone
+                        - Cheddar
+                        - Swiss
+                    Other Toppings:
+                        - Lettuce
+                        - Peppers
+                        - Onion
+                        - Tomatoes
+                        - Jalapenos
+                        - Cucumbers
+                        - Pickles
+                        - Guacamole
+                        - Mushrooms
+                    Sauces:
+                        - Mayo
+                        - Mustard
+                        - Ketchup
+                        _ Ranch
+                        - Thousand Islands
+                        - Vinaigrette
+                    """;
+
+    private Topping isToppingValid(String toppingName) {
+        Topping topping = null;
+        List<Topping> toppings = Topping.initializeToppings();
+        for (Topping t : toppings) {
+            if (t.getName().equalsIgnoreCase(toppingName)) {
+                 topping = t;
+            }
+        }
+        return topping;
     }
 }
 
